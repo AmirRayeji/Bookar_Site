8f7c28b3a19fc7b31a27804daeb5322a388152b6
Amir <amirmohamadrayeji@gmail.com>
2022-05-29 16:16:18 +0430

    setting up login page

--- account/admin.py
+++ account/admin.py
@@ -7,7 +7,13 @@ from account.models import CustomUser
 class CustomUserAdmin(UserAdmin):
     add_form=CustomUserCreationForm
     form=CustomUserChangeForm
+    list_display=['username', 'email', 'age', 'is_staff']
     model=CustomUser
-
+    fieldsets=UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets=UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
