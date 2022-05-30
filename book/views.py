fe91f0a5e55e8db231ac2386bdb770edf33a60df
Amir <amirmohamadrayeji@gmail.com>
2022-05-30 20:13:48 +0430

    added delete book page to project

--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 from book.models import Book
 
@@ -25,3 +26,10 @@ class BookUpdateView(generic.UpdateView):
     model=Book
     fields=['title', 'author', 'description', 'cover']
     template_name='book/book_update.html'
+
+
+
+class BookDeleteView(generic.DeleteView):
+    model=Book
+    template_name='book/book_delete.html'
+    success_url=reverse_lazy('book_list')
