20d0e6acfed9a14531eb90a0be36676c73bd725b
Amir <amirmohamadrayeji@gmail.com>
2022-05-30 14:40:02 +0430

    add book models and move to database

--- book/models.py
+++ book/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Book(models.Model):
+    user=models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title=models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description=models.TextField()
+    cover=models.ImageField(upload_to='book_cover/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_detail', args=[self.id])
